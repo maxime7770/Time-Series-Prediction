2e6e4f1e6dce1ff3d48d8f3506142eb0026f885c
Maxime <maxwolf34@gmail.com>
2022-01-14 23:02:40 +0100

    gitignore

--- BitcoinPrediction/preprocessing.py
+++ BitcoinPrediction/preprocessing.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -13,19 +12,3 @@ if __name__ == "__main__":
     plt.plot(close_position)
     plt.show()
 
-=======
-import pandas as pd
-import matplotlib.pyplot as plt
-
-
-data = pd.read_csv("Binance_BTCUSDT_d.csv", index_col=False)
-
-
-close_position = data["close"]
-
-
-if __name__ == "__main__":
-    plt.plot(close_position)
-    plt.show()
-
->>>>>>> 68a3b1e894bc20852fef2f9f78e8d7e5ae7e00a4
